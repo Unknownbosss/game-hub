16bec69e246ec4623ae73b0d3b980fb10752e1a1
Unknownbosss <udeigweemmanuel0@gmail.com>
2024-07-15 22:51:05 +0100

    added crirtc score

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,5 +1,6 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import PlatFormIconList from "./PlatFormIconList";
+import CriticScore from "./CriticScore";
 
 function GameCard({ game }) {
   console.log(game);
@@ -8,9 +9,12 @@ function GameCard({ game }) {
       <Image src={game?.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game?.name}</Heading>
-        <PlatFormIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+        <HStack justifyContent="space-between">
+          <PlatFormIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
