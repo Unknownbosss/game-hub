1c83f8dbbf829c3ac6c454773faacf716059b7bb
Unknownbosss <udeigweemmanuel0@gmail.com>
2024-07-15 22:42:12 +0100

    added platform icons

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,11 +1,16 @@
 import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import PlatFormIconList from "./PlatFormIconList";
 
 function GameCard({ game }) {
+  console.log(game);
   return (
     <Card borderRadius="10px" overflow="hidden">
       <Image src={game?.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game?.name}</Heading>
+        <PlatFormIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
---
+++ src/components/PlatFormIconList.jsx
@@ -0,0 +1,35 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { BsGlobe } from "react-icons/bs";
+import { HStack, Icon } from "@chakra-ui/react";
+import { TbDeviceNintendo } from "react-icons/tb";
+
+function PlatFormIconList({ platforms }) {
+  const iconMap = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: TbDeviceNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    android: FaAndroid,
+  };
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color="gray-500" />
+      ))}
+    </HStack>
+  );
+}
+
+export default PlatFormIconList;
