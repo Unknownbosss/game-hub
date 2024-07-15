8178cf79a1c000fcc38cbed26c38ffe5cb0926c0
Unknownbosss <udeigweemmanuel0@gmail.com>
2024-07-15 23:04:04 +0100

    optimize images

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,12 +1,14 @@
 import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import PlatFormIconList from "./PlatFormIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
 function GameCard({ game }) {
-  console.log(game);
+
+  console.log(getCroppedImageUrl(game.background_image));
   return (
     <Card borderRadius="10px" overflow="hidden">
-      <Image src={game?.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game?.name}</Heading>
         <HStack justifyContent="space-between">
