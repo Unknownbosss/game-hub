dff6cd60c9146fd0c5e67c2ed26dc152cb26fe9e
Unknownbosss <udeigweemmanuel0@gmail.com>
2024-07-17 00:30:13 +0100

    change color switch from text to icons

--- src/components/ColorModeSwitch.jsx
+++ src/components/ColorModeSwitch.jsx
@@ -1,11 +1,20 @@
-import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import { HStack, Switch, Text, useColorMode, Button } from "@chakra-ui/react";
+import { CiDark, CiLight } from "react-icons/ci";
 
 function ColorModeSwitch() {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <HStack>
-      <Switch colorScheme="green" isChecked={ colorMode === 'dark'} onChange={toggleColorMode}/>
-      <Text whiteSpace='nowrap'>Dark Mode</Text>
+      {/* <Switch
+        colorScheme="green"
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+      />
+      <Text whiteSpace="nowrap">Dark Mode</Text> */}
+
+      <Button color="whitesmoke" onClick={toggleColorMode}>
+        {colorMode === "dark" ? <CiLight /> : <CiDark color="gray" />}
+      </Button>
     </HStack>
   );
 }
